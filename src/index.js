dcd1bac32eea8da47a113ef04c37224490ae4887
github username <github email address>
2017-12-19 16:31:36 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+// ./src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
+      <Router>
+        <div>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/about" render={() => <h1>About</h1>} />
+        </div>
+      </Router>
     </div>
   );
 };
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById('root')
 );
